888ea188c55207357ffc84d4d1906d6287d399d3
Vasyl Kalish <vasyl.documents@yahoo.it>
2025-08-22 00:04:03 +0200

    fix flake8

--- app/main.py
+++ app/main.py
@@ -6,20 +6,18 @@ API_KEY = os.getenv("API_KEY")
 
 
 def get_weather() -> None:
-    params = {
-        "key": API_KEY,
-        "q": "paris",
-        "aqi": "no"
-    }
+    params = {"key": API_KEY, "q": "paris", "aqi": "no"}
     response = requests.get(BASE_URL, params=params)
     data_response = response.json()
-    return print(f"Performing request to Weather API for "
-                 f"city {data_response["location"]["name"]}...\n"
-                 f"{data_response["location"]["name"]}/"
-                 f"{data_response["location"]["country"]} "
-                 f"{data_response["location"]["localtime"]} "
-                 f"Weather: {data_response["current"]["temp_c"]}  "
-                 f"Celsius, {data_response["current"]["condition"]["text"]}")
+    return print(
+        f"Performing request to Weather API for "
+        f"city {data_response["location"]["name"]}...\n"
+        f"{data_response["location"]["name"]}/"
+        f"{data_response["location"]["country"]} "
+        f"{data_response["location"]["localtime"]} "
+        f"Weather: {data_response["current"]["temp_c"]}  "
+        f"Celsius, {data_response["current"]["condition"]["text"]}"
+    )
 
 
 if __name__ == "__main__":
