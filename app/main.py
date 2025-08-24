5de40d711798d85bd36377175f89a57c733536f2
Vasyl Kalish <vasyl.documents@yahoo.it>
2025-08-24 11:39:32 +0200

    add exeption for api_key is none and for responce.status code 200

--- app/main.py
+++ app/main.py
@@ -6,8 +6,12 @@ API_KEY = os.getenv("API_KEY")
 
 
 def get_weather() -> None:
+    if API_KEY is None:
+        raise Exception("API_KEY is not set, please set it to proceed")
     params = {"key": API_KEY, "q": "paris", "aqi": "no"}
     response = requests.get(BASE_URL, params=params)
+    if response.status_code != 200:
+        raise Exception(f"API request failed {response.status_code}: {response.text}")
     data_response = response.json()
     return print(
         f"Performing request to Weather API for "
